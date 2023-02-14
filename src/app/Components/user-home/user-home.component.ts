3aa8793c40cb919f9cbe77685d5d6678895ff98f
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-02-14 03:08:34 +0200

    api + file structure

---
+++ src/app/Components/user-home/user-home.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-user-home',
+  templateUrl: './user-home.component.html',
+  styleUrls: ['./user-home.component.css']
+})
+export class UserHomeComponent {
+
+}
+
+
