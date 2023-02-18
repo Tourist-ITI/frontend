a324c48fdabde8f2ded9b688b434aaed6272159f
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-02-19 01:53:09 +0200

    user-home Page Finished

--- src/app/Container/home-page/destination/destination.component.ts
+++ src/app/Container/home-page/destination/destination.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class DestinationComponent {
   cards = [{ src: "../../../assets/Images/home/Cairo-5.jpg", caption: "Cairo" },
-  { src: "../../../assets/Images/home/alexandria-3.jpg", caption: "Alex" },
+  { src: "../../../assets/Images/home/Alexandria-3.jpg", caption: "Alex" },
   { src: "../../../assets/Images/home/Fayoum.jpeg", caption: "Fayoum" },
   { src: "../../../assets/Images/home/Port-said.jfif", caption: "Port Said" }]
 }
