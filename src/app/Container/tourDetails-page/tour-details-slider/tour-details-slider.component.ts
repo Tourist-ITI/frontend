b4ce0935fc07247cfd0a22e9a14ab6a424bc6a3d
Maii <maimoustafa9821@gmail.com>
2023-02-21 02:05:04 +0200

    v1

--- src/app/Container/tourDetails-page/tour-details-slider/tour-details-slider.component.ts
+++ src/app/Container/tourDetails-page/tour-details-slider/tour-details-slider.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tour-details-slider',
   templateUrl: './tour-details-slider.component.html',
-  styleUrls: ['./tour-details-slider.component.css']
+  styleUrls: ['./tour-details-slider.component.css'],
 })
-export class TourDetailsSliderComponent {
-
-}
+export class TourDetailsSliderComponent {}
